05e008c47c2745701f4040dd43a2b935d04ab32f
Kyle Volle <kvolle3@gatech.edu>
2019-05-16 18:37:00 -0500

    Rewrote just about everything. Need to add the ability to use tfrecords by copying helper functions and making the constructor pass the tensors (removing placeholders)

--- attempt.py
+++ attempt.py
@@ -3,80 +3,102 @@ import numpy as np
 from tensorflow.contrib.slim.nets import resnet_v2
 from tensorflow.contrib.framework.python.ops import arg_scope
 
-def fcl(input, nodes, name):
-    # Pass through to conv_layer. renamed function for easier readability
-    return conv_layer(input, [1, 1, input.shape[3], nodes], name , padding='VALID', stride=1, pooling=False)
+class network:
+    def __init__(self):
+        with arg_scope(resnet_v2.resnet_arg_scope()) as scope:
+            with tf.variable_scope("", reuse=tf.AUTO_REUSE) as scope:
+                self.inputs1 = tf.placeholder(tf.float32, shape=(None, 240, 192, 3))
+                self.inputs2 = tf.placeholder(tf.float32, shape=(None, 240, 192, 3))
+                self.match = tf.placeholder(tf.bool, shape=(None))
+                out1 = self.side(self.inputs1)
+                scope.reuse_variables()
+                out2 = self.side(self.inputs2)
 
-def conv_layer(input_layer, weights, name, padding, stride=1, pooling=True):
-    # with tf.variable_scope(name) as scope:
-    kernel = tf.get_variable(name+"_kernel", shape=weights, dtype=tf.float32, initializer=tf.truncated_normal_initializer(stddev=0.1, dtype=tf.float32))
-    conv = conv2d(input_layer, kernel, padding, stride)
-    init = tf.constant(1., shape=[weights[-1]], dtype=tf.float32)
-    bias = tf.get_variable(name+"_bias",  dtype=tf.float32, initializer=init)
-    preactivation = tf.nn.bias_add(conv, bias)
-    conv_relu = tf.nn.relu(preactivation, name=name)
+                self.diff = tf.norm(tf.subtract(out1, out2), name="diff")
+                self.margin = 25.0
 
-    if pooling:
-        out = create_max_pool_layer(conv_relu)
-    else:
-        out = conv_relu
-    return out
+    def fcl(self, input_layer, nodes, name):
+        # Pass through to conv_layer. renamed function for easier readability
+        layer = self.conv_layer(input_layer, [1, 1, input_layer.shape[3], nodes], name, padding='VALID', stride=1, pooling=False)
+        return layer
+
+    def conv_layer(self, input_layer, weights, name, padding, stride=1, pooling=True):
+        # with tf.variable_scope(name) as scope:
+        kernel = tf.get_variable(name+"_kernel", shape=weights, dtype=tf.float32, initializer=tf.truncated_normal_initializer(stddev=0.1, dtype=tf.float32))
+        conv = self.conv2d(input_layer, kernel, padding, stride)
+        init = tf.constant(1., shape=[weights[-1]], dtype=tf.float32)
+        bias = tf.get_variable(name+"_bias",  dtype=tf.float32, initializer=init)
+        preactivation = tf.nn.bias_add(conv, bias, name=name+"_bias_add")
+        conv_relu = tf.nn.relu(preactivation, name=name)
+
+        if pooling:
+            out = self.create_max_pool_layer(conv_relu)
+        else:
+            out = conv_relu
+        return out
 
 
-def conv2d(input_layer, W, pad, stride=1):
-    return tf.nn.conv2d(input=input_layer,
-                        filter=W,
-                        strides=[1, stride, stride, 1],
-                        padding=pad)
+    def conv2d(self, input_layer, W, pad, stride=1):
+        return tf.nn.conv2d(input=input_layer,
+                            filter=W,
+                            strides=[1, stride, stride, 1],
+                            padding=pad)
 
-def create_max_pool_layer(input):
-    return tf.nn.max_pool(value=input,
-                          ksize=[1, 2, 2, 1],
-                          strides=[1, 2, 2, 1],
-                          padding='SAME')
-def network(input):
-    net1, end_points1 = resnet_v2.resnet_v2_101(input, None, is_training=True, global_pool=False, output_stride=16)
-    arranged = tf.reshape(net1, shape=[-1, 1, 1, 3 * 4 * 2048])
-    fc1 = fcl(arranged, 1024, "fc1")
-    fc2 = fcl(fc1, 2048, "fc2")
-    fc3 = fcl(fc2, 512, "fc3")
-    return fc3
+    def create_max_pool_layer(self, input):
+        return tf.nn.max_pool(value=input,
+                              ksize=[1, 2, 2, 1],
+                              strides=[1, 2, 2, 1],
+                              padding='SAME')
+    def side(self, input):
+        net1, end_points1 = resnet_v2.resnet_v2_101(input, None, is_training=True, global_pool=False, output_stride=16)
+        arranged = tf.reshape(net1, shape=[-1, 1, 1, 12 * 15 * 2048], name="arrange_for_fcl")
+        self.fc1 = self.fcl(arranged, 1024, "fc1")
+        self.fc2 = self.fcl(self.fc1, 2048, "fc2")
+        self.fc3 = self.fcl(self.fc2, 512, "fc3")
+        return self.fc3
 
-with arg_scope(resnet_v2.resnet_arg_scope()):
-    with tf.variable_scope("core", reuse=tf.AUTO_REUSE) as scope:
-        inputs1 = tf.placeholder(tf.float32, shape = (None,48, 64, 3))
-        inputs2 = tf.placeholder(tf.float32, shape = (None,48, 64, 3))
+    def loss(self):
+        self.distance_matching = tf.multiply(self.match,self.diff,name="distance_matching")
+        non_match = tf.subtract(tf.constant(self.margin, dtype=tf.float32, name="margin"), self.diff, name="margin_minus_diff")
+        self.distance_unmatched = tf.multiply(non_match, self.diff, name = "distance_unmatched")
 
-        out1 = network(inputs1)
-        scope.reuse_variables()
-        out2 = network(inputs2)
+        return tf.reduce_sum(self.distance_matching)+ tf.reduce_sum(self.distance_unmatched)
 
-        diff = tf.norm(tf.subtract(out1, out2))
 
-saver = tf.train.Saver()
+network = network()
+
 with tf.Session() as sess:
-    #variables_can_be_restored = list(set(tf.get_collection_ref(tf.GraphKeys.GLOBAL_VARIABLES)).intersection(tf.train.list_variables("./model/")))
-    #variables_can_be_restored = list(tf.train.list_variables("./model/"))
-    g = tf.Graph()
+    tf.initialize_all_variables().run()
     variables_can_be_restored = tf.train.list_variables("./model/")
+    list_of_variables = []
+    for v in variables_can_be_restored:
+        list_of_variables.append(v[0]+":0")
+        #print(v[0]+":0")
+    #print("Global~~~")
+    globals=[]
+    for v in tf.global_variables():
+        globals.append(v.name)
+        #print(v.name)"""
+
+    #reduced_list = list(set(tf.get_collection_ref(tf.GraphKeys.GLOBAL_VARIABLES)).intersection(set(list_of_variables)))
+    reduced_list = list(set(globals).intersection(list_of_variables))
+    pretrained_vars = tf.contrib.framework.get_variables_to_restore(include=reduced_list)
+    #pretrained_vars = tf.contrib.framework.get_variables_to_restore(include=["resnet_v2_101"])
+
+    tf_pretrained_saver = tf.train.Saver(pretrained_vars, name="pretrained_saver")
+    tf_saver = tf.train.Saver(name="saver")
+    tf_pretrained_saver.restore(sess, "./model/model.ckpt-30452")
+    """
     vars = []
     for v in variables_can_be_restored:
-        name = v[0]
-        var = g.get_tensor_by_name(name)
-        vars.append(var)
-    #for v in variables_can_be_restored: # tf.global_variables():
-    #    print (v)
-    temp_saver = tf.train.Saver(vars)
+        var_name = v[0]
+        print(var_name)
     """
-    ckpt_state = tf.train.get_checkpoint_state('./model/')
-    print('Loading checkpoint %s' % ckpt_state.model_checkpoint_path)
-    temp_saver.restore(sess, ckpt_state.model_checkpoint_path)"""
-
     writer = tf.summary.FileWriter("log/", sess.graph)
     writer.close()
-    tf.initialize_all_variables().run()
-    test1 = np.ones([1, 48, 64, 3])
-    test2 = np.zeros([1, 48, 64, 3])
-    test = sess.run(diff,feed_dict={inputs1:test1, inputs2:test2})
+
+    test1 = np.ones([1, 240, 192, 3])
+    test2 = np.zeros([1, 240, 192, 3])
+    test = sess.run(network.diff,feed_dict={network.inputs1:test1, network.inputs2:test2})
     print(test)
     print("Fin")
